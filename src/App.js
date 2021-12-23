fac8efe5c05cd20bee0ca1163206565b98ad5f4d
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-12-22 19:39:14 -0800

    add about compontent

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Header from './components/Header'
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import About from './components/AboutMe';
 
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <About />
       <Projects />
-      <Resume />
       <Contact />
     </div>
   );
