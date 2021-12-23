712e5cedaf336bda23d9b2d846c5ccdef96a9b8d
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-12-22 19:53:00 -0800

    add about content and style

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,8 @@
 const About = () => {
     return (
-        <div>
-            
+        <div className="about-container">
+            <h1>Hello, I'm Caleb Stevens.</h1>
+            <h3>I'm a full-stack web developer.</h3>
         </div>
     );
 }
