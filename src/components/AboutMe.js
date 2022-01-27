d9d79b2bd8d96a4a16d3760aff7926d0516a87fd
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-01-26 21:57:19 -0800

    Added arrow pointing down to about

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,13 @@ const About = () => {
         <div className="about-container">
             <p className="about-p-1">Hello, I'm Caleb Stevens.</p>
             <p className="about-p-2">I'm a full-stack web developer.</p>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down-circle" width="88" height="88" viewBox="0 0 24 24" stroke-width="2.5" stroke="#274C77" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <line x1="8" y1="12" x2="12" y2="16" />
+            <line x1="12" y1="8" x2="12" y2="16" />
+            <line x1="16" y1="12" x2="12" y2="16" />
+            </svg>
         </div>
     );
 }
