df1d37d450cdb1edbc8e1e9269ee0276819defe9
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-11-12 20:40:25 -0800

    Adds next config

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+require('dotenv').config();
+
+const resolveRoot = (newPath) => {
+  return path.resolve(__dirname, newPath);
+}
+
+module.exports = {
+  webpack: config => {
+    const resolveAlias = config.resolve.alias;
+    config = {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...resolveAlias,
+        },
+      }
+    };
+    config.devtool = process.env.NODE_ENV !== 'production' && 'eval-source-map';
+    return config;
+  }
+}
