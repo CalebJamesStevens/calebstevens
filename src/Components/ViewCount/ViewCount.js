3fd3f9d60fbd66d6a325e8411d3aea78fa0423fb
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-11-20 21:14:01 -0800

    added viewcounter

---
+++ src/Components/ViewCount/ViewCount.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import useSWR from 'swr';
+
+/** MUI Components */
+import { Typography } from '@mui/material';
+
+/** Icons */
+import { Visibility } from '@mui/icons-material';
+
+/** Styles */
+import styles from './styles';
+
+async function fetcher(...args) {
+  const res = await fetch(...args);
+
+  return res.json();
+}
+
+export default function ViewCount({ slug }) {
+  const { data } = useSWR(`/api/views/${slug}`, fetcher);
+  const views = new Number(data?.total);
+
+  return (
+    <Typography sx={styles.text}>
+      <Visibility />
+      {views > 0 ? views.toLocaleString() : '0'}
+    </Typography>
+  );
+}
---
+++ src/Components/ViewCount/styles.js
@@ -0,0 +1,9 @@
+export default {
+  text: {
+    '&.MuiTypography-root': {
+      display: 'flex',
+      gap: (theme) => theme.spacing(1),
+      padding: (theme) => theme.spacing(1),
+    },
+  },
+};
