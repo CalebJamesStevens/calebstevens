56108fc449fbd6f4a7284bb756b53e55b2f9256d
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-11-20 22:19:07 -0800

    Adds load spinner for view count

--- src/Components/ViewCount/styles.js
+++ src/Components/ViewCount/styles.js
@@ -4,6 +4,13 @@ export default {
       display: 'flex',
       gap: (theme) => theme.spacing(1),
       padding: (theme) => theme.spacing(1),
+      alignItems: 'center',
+    },
+  },
+  circularProgress: {
+    '&.MuiCircularProgress-root': {
+      width: '20px !important',
+      height: '20px !important',
     },
   },
 };
